56c8961b8cd62005af52d695752930acd09fd388
bighandsdev <63073747+bighandsdev@users.noreply.github.com>
2021-05-31 22:08:52 +0100

    minor tweaks

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -5,11 +5,7 @@ const Helper = artifacts.require("./Helper.sol");
 const WETHGateway = artifacts.require("./WETHGateway.sol");
 const PromTest = artifacts.require("./test/PromTest.sol");
 const ShareCalulator = artifacts.require("./lib/math/ShareCalculator.sol");
-
-var uni = "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D";
-var dai = "0xff795577d9ac8bd7d90ee22b6c1703490b6512fd";
 var weth = "0xd0a1e359811322d97991e03f863a0c30c2cf029c";
-var me = "0x1E67cC1199F312d495694D99C57a3667fBb6FCe5";
 
 module.exports = async function (deployer, accounts) {
   await deployer.deploy(ShareCalulator);
