012e9188fe0adfaf95009691776f2adf2183b146
PSYTWEAK <psy@promise.exchange>
2021-09-29 15:04:26 +0100

    delete

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -15,7 +15,7 @@ module.exports = async function (deployer, accounts) {
   await deployer.link(ShareCalulator, PromiseCore);
   await deployer.deploy(
     PromiseCore,
-    "0x1E67cC1199F312d495694D99C57a3667fBb6FCe5"
+    "0xA7b4eD54ec0bD114D3A69bc97d9ce8CBcc09F45F"
   );
 
   await deployer.deploy(PromiseToken);
@@ -24,63 +24,6 @@ module.exports = async function (deployer, accounts) {
   await deployer.deploy(PromiseFinder, promiseCore.address);
   const promiseFinder = await PromiseFinder.deployed();
   const promiseToken = await TestToken.deployed();
-  await deployer.deploy(
-    PromiseChef,
-    promiseToken.address,
-    promiseCore.address,
-    "1000000000000000000",
-    "100",
-    "30966004"
-  );
-  const promiseChef = await PromiseChef.deployed();
-  await deployer.deploy(
-    PromiseHolder,
-    promiseCore.address,
-    promiseChef.address
-  );
-  const promiseHolder = await PromiseHolder.deployed();
-  await deployer.deploy(
-    PromiseChefFinder,
-    promiseCore.address,
-    promiseChef.address,
-    promiseHolder.address
-  );
-  const promiseChefFinder = await PromiseChefFinder.deployed();
-  await promiseToken.mint(promiseChef.address, "100000000000000000000000");
-  await promiseChef.setPromiseHolder(promiseHolder.address);
-
-  let allocationPoints = "10000";
-  let creatorToken = promiseToken.address;
-  let joinerToken = "0x7ceb23fd6bc0add59e62ac25578270cff1b9f619";
-  let minUncalculatedRatio = ["1", "100"];
-  let maxUncalculatedRatio = ["1", "70"];
-  let updatePool = true;
-  let expirationDate = "1634554704";
-  await promiseChef.add(
-    allocationPoints,
-    creatorToken,
-    joinerToken,
-    minUncalculatedRatio,
-    maxUncalculatedRatio,
-    updatePool,
-    expirationDate
-  );
-  allocationPoints = "20000";
-  creatorToken = promiseToken.address;
-  joinerToken = "0x8f3cf7ad23cd3cadbd9735aff958023239c6a063";
-  minUncalculatedRatio = ["1", "2000"];
-  maxUncalculatedRatio = ["1", "1000"];
-  updatePool = true;
-  expirationDate = "1634554704";
-  await promiseChef.add(
-    allocationPoints,
-    creatorToken,
-    joinerToken,
-    minUncalculatedRatio,
-    maxUncalculatedRatio,
-    updatePool,
-    expirationDate
-  );
 
   await deployer.deploy(Helper, promiseCore.address, promiseChef.address);
   const helper = await Helper.deployed();
